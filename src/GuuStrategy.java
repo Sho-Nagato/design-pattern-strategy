06358292228b2884de4665428d0c5dd3f2272953
sho.nagato <s.nagato-good-works.co.jp>
2021-02-17 17:56:47 +0900

    first commit

---
+++ src/GuuStrategy.java
@@ -0,0 +1,7 @@
+public class GuuStrategy implements Strategy {
+
+    @Override
+    public Hand nextHand() {
+        return Hand.getHand(Hand.HANDVALUE_GUU);
+    }
+}
---
+++ src/RandomStrategy.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomStrategy implements Strategy {
+
+    @Override
+    public Hand nextHand() {
+        Random random = new Random();
+        return Hand.getHand(random.nextInt(3));
+    }
+}
